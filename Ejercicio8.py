efbbf27da6ff73c7d45e471ddcf5e25514cd20a9
Sergio <sergio.gonzalez@iesnervion.es>
2025-09-29 18:55:32 +0200

    9

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -1,14 +1,18 @@
 #Solicita al usuario un número n y dibuja un triángulo de base y altura n. Por ejemplo para n=4 debe dibujar lo siguiente:
+   #*
+  #* *
 
 #pedimos número al usuario y lo guardamos en una variable num
 num=int(input("Introduce un número entero positivo"))
+  
 
-for n in range(1,num+1):
-    
-    for n in range(1,num):
-        print(" ", end="")
-    print("*")
+   
+for i in range(1, num + 1):
+    # Espacios iniciales para centrar la pirámide
+    print(" " * (num - i), end="")
+    # Asteriscos con espacio entre ellos
+    print("* " * i)
 
 
   
-   
+    
